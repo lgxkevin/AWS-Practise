1c6e3cbbd20c5858ce4fd8936576cc101173426d
lgxkevin <lgx9587@gmail.com>
2019-10-14 21:55:18 +0800

    conditional update

--- AWS-demos/DynamoDB/sandbox/conditional.js
+++ AWS-demos/DynamoDB/sandbox/conditional.js
@@ -3,26 +3,48 @@ aws.config.update({region: 'us-east-2'});
 
 const docClient = new AWS.DynamoDB.DocumentClient();
 
-docClient.put({
-    TableName: 'td_notes',
-    Item: {
-        user_id: 'ABC',
-        timestamp: 1,
-        title: 'New Title',
-        content: 'New Content'
-    },
-    // https://docs.aws.amazon.com/en_pv/amazondynamodb/latest/developerguide/Expressions.OperatorsAndFunctions.html
-    ConditionExpression: '#t <> :t',
-    ExpressionAttributeNames: {
-        '#t': 'timestamp'
-    },
-    ExpressionAttributeValues: {
-        ':t': 1
-    }
-}, (err, data)=>{
-    if(err) {
-        console.log(err);
-    } else {
-        console.log(data);
-    }
+// https://docs.aws.amazon.com/en_pv/amazondynamodb/latest/developerguide/Expressions.OperatorsAndFunctions.html
+
+// docClient.put({
+//     TableName: 'td_notes',
+//     Item: {
+//         user_id: 'ABC',
+//         timestamp: 1,
+//         title: 'New Title',
+//         content: 'New Content'
+//     },
+//     ConditionExpression: '#t <> :t',
+//     ExpressionAttributeNames: {
+//         '#t': 'timestamp'
+//     },
+//     ExpressionAttributeValues: {
+//         ':t': 1
+//     }
+// }, (err, data)=>{
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log(data);
+//     }
+// });
+
+docClient.update({
+  TableName: 'td_notes',
+  Key: {
+      user_id: 'ABC',
+      timestamp: 1
+  },
+  UpdateExpression: 'set #v = #v + :incr',
+  ExpressionAttributeNames: {
+      '#v': 'views'
+  },
+  ExpressionAttributeValues: {
+      ':incr': 1
+  }
+}, (err, data)=> {
+  if(err) {
+      console.log(err);
+  } else {
+      console.log(data);
+  }
 });
